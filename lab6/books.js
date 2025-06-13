f3212257b4e47ab3b8e8021e42428b87712f626e
Конев Станислав <konevs2945@MAIL.RU>
2025-06-13 21:01:31 +0700

    книги

---
+++ lab6/books.js
@@ -0,0 +1,81 @@
+const books = [
+    { 
+        author: 'Харуки Мураками',
+        name: 'Норвежский лес', 
+        year: 1987, 
+        price: 520, 
+        bestseller: true, 
+        inStock: false 
+    },
+    { 
+        author: 'Рэй Брэдбери', 
+        name: '451 градус по Фаренгейту', 
+        year: 1953, 
+        price: 470, 
+        bestseller: true, 
+        inStock: true 
+    },
+    { 
+        author: 'Умберто Эко', 
+        name: 'Имя розы', 
+        year: 1980, 
+        price: 610, 
+        bestseller: true, 
+        inStock: true 
+    },
+    { 
+        author: 'Стивен Кинг', 
+        name: 'Сияние', 
+        year: 1977, 
+        price: 430, 
+        bestseller: true, 
+        inStock: false 
+    },
+    { 
+        author: 'Милан Кундера', 
+        name: 'Невыносимая лёгкость бытия', 
+        year: 1984, 
+        price: 390, 
+        bestseller: false, 
+        inStock: true 
+    },
+    { 
+        author: 'Чак Паланик', 
+        name: 'Бойцовский клуб', 
+        year: 1996,
+        price: 360, 
+        bestseller: false, 
+        inStock: true 
+    },
+    { 
+        author: 'Жозе Сарамаго', 
+        name: 'Слепота', 
+        year: 1995, 
+        price: 410, 
+        bestseller: false, 
+        inStock: false 
+    },
+    { 
+        author: 'Пауло Коэльо', 
+        name: 'Алхимик', 
+        year: 1988, 
+        price: 450, 
+        bestseller: true, 
+        inStock: true 
+    },
+    { 
+        author: 'Маргарет Этвуд', 
+        name: 'Рассказ служанки', 
+        year: 1985, price: 490, 
+        bestseller: true, 
+        inStock: false 
+    },
+    { 
+        author: 'Оскар Уайльд', 
+        name: 'Портрет Дориана Грея', 
+        year: 1890, 
+        price: 370, 
+        bestseller: false, 
+        inStock: true 
+    }
+];
